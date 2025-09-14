7e7c7105cc5c1aa38c14d587495ceb67612a795d
Ville Kr <ville.karkkainen@outlook.com>
2025-09-14 18:05:06 +0300

    chore: perf tweak

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,6 @@
 import asyncio
 import time
-from typing import Optional
+from typing import Optional, Union
 
 from aioboto3 import Session
 from botocore.config import Config
@@ -8,7 +8,7 @@ from botocore.config import Config
 # Reuse the aioboto3 session between Lambda invocations to reduce overhead
 _SESSION = Session()
 # Increase connection pool size to improve throughput with concurrent S3 requests
-_S3_CONFIG = Config(max_pool_connections=64)
+_S3_CONFIG = Config(max_pool_connections=256)
 
 
 def lambda_handler(event, context):
@@ -18,7 +18,7 @@ def lambda_handler(event, context):
 
     return {
         "lang": "python",
-        "detail": "aiobotocore",
+        "detail": "aioboto3",
         "result": result,
         "time": elapsed,
     }
@@ -29,7 +29,7 @@ async def processor(event):
     max_concurrency = int(event.get("concurrency", 64))
     sem = asyncio.Semaphore(max_concurrency)
 
-    async with _SESSION.create_client("s3", config=_S3_CONFIG) as s3:
+    async with _SESSION.client("s3", config=_S3_CONFIG) as s3:
         bucket_name = event["s3_bucket_name"]
         folder = event["folder"]
         find = event["find"]
@@ -46,7 +46,7 @@ async def processor(event):
             return str(len(keys))
 
 
-async def get(s3, bucket_name: str, key: str, find: str, sem: asyncio.Semaphore) -> Optional[str]:
+async def get(s3, bucket_name: str, key: str, find: Optional[Union[str, bytes]], sem: asyncio.Semaphore) -> Optional[str]:
     async with sem:
         response = await s3.get_object(Bucket=bucket_name, Key=key)
         body_bytes = await response["Body"].read()  # fully read the body as required
@@ -55,7 +55,7 @@ async def get(s3, bucket_name: str, key: str, find: str, sem: asyncio.Semaphore)
             if isinstance(find, str):
                 find_bytes = find.encode("utf-8")
             else:
-                # Just in case the caller provided bytes already
-                find_bytes = bytes(find)
+                # find is bytes here
+                find_bytes = find
             return key if body_bytes.find(find_bytes) != -1 else None
         return None
